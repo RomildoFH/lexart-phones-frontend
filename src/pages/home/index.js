cfa0ef814092679392ff9cb6ea2951e1288bc22b
Romildo <romildo.silvafilho1@gmail.com>
2024-03-05 21:54:45 -0300

    style: overflow products list

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -84,9 +84,8 @@ const Home = () => {
               />
               <img src={Filter} alt={`filter-icon.svg`} className={`hover:brightness-125 transition duration-300 cursor-pointer`} />
             </section>
-            <section className={`w-full border-2 border-gray-50 rounded-lg py-8`}>
-              <ul className={`flex-col w-full max-h-96 bg-coral border-b border-gray-50 mb-2 overflow-y-scroll`}>
-                <li className="grid grid-cols-[0.5fr,2fr,1fr,1fr,1fr,1fr,0.2fr,0.2fr] gap-4 w-full font-medium mb-4 bg-white h-fit border border-gray-50 py-4 px-6">
+            <section className={`w-full border-2 border-gray-50 rounded-lg py-8 pr-2`}>
+                <li className="grid grid-cols-[0.5fr,2fr,1fr,1fr,1fr,1fr,0.2fr,0.2fr] gap-4 w-full font-medium mb-4 bg-white h-fit border-y border-gray-50 py-4 px-6">
                   <div className={`flex justify-between w-fit gap-2 hover:brightness-125 cursor-pointer`}>
                     <p className="overflow-hidden text-left">Item</p>
                     <img src={DownArrow} alt={`ordenar por item`} onClick={() => orderProducts('index')} />
@@ -112,35 +111,38 @@ const Home = () => {
                     <img src={DownArrow} alt={`ordenar por item`} onClick={() => orderProducts('price')} />
                   </div>
                 </li>
+              <div className={`overflow-y-scroll pr-2 max-h-72`}>
+                <ul className={`flex-col w-full h-full bg-coral border-b border-gray-50 mb-2`}>
                   {
-                  productList?.filter((e, index) => listSize ? index < listSize : filtredList.length).map((product, index) => (
-                    <li className="grid grid-cols-[0.5fr,2fr,1fr,1fr,1fr,1fr,0.2fr,0.2fr] gap-4 w-full bg-white h-fit border-t border-gray-50 py-4 px-6">
-                      <p className="overflow-hidden text-left">{index + 1}</p>
-                      <p className="overflow-hidden text-left">{product.name}</p>
-                      <p className="overflow-hidden text-left">{product.brand}</p>
-                      <p className="overflow-hidden text-left">{product.model}</p>
-                      <p className="overflow-hidden text-left">{product.color}</p>
-                      <p
-                      className="overflow-hidden text-left"
-                      >
-                        {
-                          Number(product.price).toLocaleString(
-                            'pt-BR', { style: 'currency', currency: 'BRL' }
-                          )
-                        }
-                      </p>
-                      <img
-                        src={EditIcon} alt={`editar`} 
-                        className={`hover:brightness-200 transition duration-300 cursor-pointer`}
-                      />
-                      <img
-                        src={DeleteIcon} alt={`deletar`}
-                        className={`hover:brightness-200 transition duration-300 cursor-pointer`}
-                      />
-                    </li>
-                  ))
-                }
-              </ul>
+                    productList?.filter((e, index) => listSize ? index < listSize : filtredList.length).map((product, index) => (
+                      <li className="grid grid-cols-[0.5fr,2fr,1fr,1fr,1fr,1fr,0.2fr,0.2fr] gap-4 w-full bg-white h-fit border-t border-gray-50 py-4 px-6">
+                        <p className="overflow-hidden text-left">{index + 1}</p>
+                        <p className="overflow-hidden text-left">{product.name}</p>
+                        <p className="overflow-hidden text-left">{product.brand}</p>
+                        <p className="overflow-hidden text-left">{product.model}</p>
+                        <p className="overflow-hidden text-left">{product.color}</p>
+                        <p
+                        className="overflow-hidden text-left"
+                        >
+                          {
+                            Number(product.price).toLocaleString(
+                              'pt-BR', { style: 'currency', currency: 'BRL' }
+                            )
+                          }
+                        </p>
+                        <img
+                          src={EditIcon} alt={`editar`} 
+                          className={`hover:brightness-200 transition duration-300 cursor-pointer`}
+                        />
+                        <img
+                          src={DeleteIcon} alt={`deletar`}
+                          className={`hover:brightness-200 transition duration-300 cursor-pointer`}
+                        />
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
               <div className={`flex pl-4 pt-4 gap-4 align-middle h-10`}>
                 <img src={LeftArrow} alt={`back-page`} className={`hover:brightness-125 transition duration-300 cursor-pointer`} />
                 <p>{`Página ${1} de ${pages}`}</p>
